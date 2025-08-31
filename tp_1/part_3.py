9073b5a900aec93f34c25948a2a3077e65bac942
claudio Molina <molina.claudio@estudiantes.unahur.edu.ar>
2025-08-30 21:26:06 -0300

    correcciones

--- tp_1/part_3.py
+++ tp_1/part_3.py
@@ -35,12 +35,11 @@ def part_3():
     evaluated = []
     # print(test[0])
     # discrete_naive_bayes(test[0], train, attrs, concepto, condicion_cumplida)
-    # discrete_naive_bayes2(test[0], train, attrs, concepto, condicion_cumplida)
     for row in test:
         result = discrete_naive_bayes(row, train, attrs, concepto, condicion_cumplida)
         row[prediction_column] = result
         evaluated.append(row)
-    
+
     confusion_matrix_result = confusion_matrix(
         evaluated, concepto, prediction_column, condicion_cumplida
     )
@@ -72,7 +71,7 @@ def discrete_naive_bayes(test_row, train, attrs, concepto_column, condicion_cump
     pi = {}
     theta = {}
     laplace = 1
-    num_vals_attr = {}
+    types_vals_attr = {}
 
     for attr in attrs:
         value = test_row[attr]
@@ -85,54 +84,61 @@ def discrete_naive_bayes(test_row, train, attrs, concepto_column, condicion_cump
             pi[row[concepto_column]] = 0
 
         class_rows[row[concepto_column]].append(row)
-        
+
         for attr in attrs:
-            value = row[attr]
-            if attr not in num_vals_attr:
-                num_vals_attr[attr] = set()
-            num_vals_attr[attr].add(value)
+            value_row = row[attr]
+            if attr not in types_vals_attr:
+                types_vals_attr[attr] = set()
+            types_vals_attr[attr].add(value_row)
 
-        for value in theta:
-            if value in row.values():
-                theta[value]["total"] += 1
-                theta[value][row[concepto_column]] = (
-                    theta[value][row[concepto_column]] + 1
-                    if row[concepto_column] in theta[value]
-                    else 1
-                )
+            for value_t in theta:
+                if value_t == value_row and theta[value_t]["attr"] == attr:
+                    theta[value_t]["total"] += 1
+                    theta[value_t][row[concepto_column]] = (
+                        theta[value_t][row[concepto_column]] + 1
+                        if row[concepto_column] in theta[value_t]
+                        else 1
+                    )
+                    break
 
-    for classes in pi:
+    for K in pi:
         # print(len(class_rows[classes]), classes, "total de registros de Cada")
-        numerador = len(class_rows[classes]) + laplace
-        denominador = N + laplace * len(class_rows)
-        pi[classes] = numerador / denominador
+        total_items_in_class = len(class_rows[K])
+        numerador = total_items_in_class + laplace
+        
+        total_Ks = len(class_rows)
+        denominador = N + laplace * total_Ks
+        
+        pi[K] = numerador / denominador
 
     # print(pi, "% de cada")
     # print(theta)
-    total_p = 0
-    for classes in pi:
+    total_probability = 0
+    probability = {}
+    for K in pi:
         producto = 1
         for value in theta:
-            count = theta[value].get(classes, 0)
+            count = theta[value].get(K, 0)
             numerador = count + laplace
 
-            correccion_laplace = laplace * len(num_vals_attr[theta[value].get("attr")])
-            N_register_class = len(class_rows[classes])
+            quantity_values_from_attr = len(types_vals_attr[theta[value].get("attr")])
+            N_register_class = len(class_rows[K])
 
-            denominador = N_register_class + correccion_laplace
+            denominador = N_register_class + laplace * quantity_values_from_attr
+
             producto *= numerador / denominador
 
         # print(producto, classes, "PRODUCTO")
-        pi[classes] *= producto
-        total_p += pi[classes]
+        probability[K] = pi[K] * producto
+        total_probability += probability[K]
     # print(pi)
     accept = 0
     reject = 0
-    for classes in pi:
-        if classes == condicion_cumplida:
-            accept = pi[classes] / total_p
+    for K in probability:
+        if K == condicion_cumplida:
+            accept = probability[K] / total_probability
         else:
-            reject = pi[classes] / total_p
+            reject = probability[K] / total_probability
 
     # print(accept, reject)
     return accept if accept > reject else abs(reject - 1)
